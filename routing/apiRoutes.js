870d0c6d8694b5f4409048e0573f6a867c81396e
Your Name <you@example.com>
2018-05-02 07:14:24 -0400

    finding the total difference in score between existing friends and new friend in app.post

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,27 +1,69 @@
 //dependencies
-var uuid = require ("uuid/v4");
+var uuid = require("uuid/v4");
 
 //data
 var friends = require("../app/data/friends");
 
-module.exports = function(app){
-    
-    //get all the user profile and send back json
-    app.get("/api/friends", function(req, res){
-        res.json(friends);
-    });
+module.exports = function (app) {
 
-    //post new profiles to the server
-    app.post("/api/friends/new", function(req, res){
+	//get all the user profile and send back json
+	app.get("/api/friends", function (req, res) {
+		res.json(friends);
+	});
 
-        //get info from POST request
-        var friend = req.body;
+	//post new profiles to the server
+	app.post("/api/friends/new", function (req, res) {
 
-        friend.id = uuid();
+		//get info from POST request
+		var friend = req.body;
 
-        friends.push(friend);
+		friend.id = uuid();
 
-        res.json(friend);
-        console.log(friend);
-    })
+		friends.push(friend);
+
+		res.json(friend);
+		console.log(friend);
+		findFriend(friend);
+	});
+
+	function findFriend(friend) {
+
+		//to store the new friend's score in an array
+		var scoreOne = [];
+		scoreOne = friend.score;
+
+		//to get the existing friends score in an array to compare
+		var diffArr = [];
+		var otherFriends = [];
+
+		for (var i = 0; i < friends.length; i++) {
+
+			//to make sure the newfriend is not compared against themself
+			if (friends[i].id !== friend.id) {
+				otherFriends.push(friends[i]);
+				
+				var scoreTwo = [];
+				scoreTwo = friends[i].scores;
+
+				// to find the difference in score between the newfriend and each of the existing friends
+				var totalDiff = 0;
+				for (var j = 0; j < scoreOne.length; j++) {
+					var diff = Math.abs((scoreOne[j] - scoreTwo[j]));
+					totalDiff += diff;
+					otherFriends[i].totalDiff = totalDiff;
+				}
+				//to create a totalDiff property for each of the friends and add the totalDiff value
+				
+			}
+		}
+		console.log(otherFriends);
+		// to Sort the otherFriends array by totalDiff to find the closest score
+		// otherFriends.sort(function (a, b) {
+		// return (a.totalDiff - b.totalDiff);
+		// console.log(otherFriends);
+
+		// });
+
+	}
+
 }
